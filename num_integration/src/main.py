b2ce9c01760a5303235d55e7b757f9eeb1c02000
Ivan Mihun <vsk76090@gmail.com>
2024-11-01 15:13:28 +0100

    feat(num_integration): add trapezoidal integration

--- num_integration/src/main.py
+++ num_integration/src/main.py
@@ -5,8 +5,12 @@ def simpson_integration():
     ...
 
 
-def trapezoidal_integration():
-    ...
+def trapezoidal_integration(func: Callable[[float], float], a: float, b: float, n: int) -> float:
+    h = (b - a) / n
+    integral = func(a) + func(b) / 2
+    for i in range(1, n):
+        integral += func(a + i * h)
+    return integral * h
 
 
 def rectangular_integration(func: Callable[[float], float], a: float, b: float, n: int) -> float:
@@ -19,6 +23,7 @@ def rectangular_integration(func: Callable[[float], float], a: float, b: float, 
 
 def main():
     print(f"{rectangular_integration(lambda x: 4 - x ** 2, 0, 2, 1000)=}")
+    print(f"{trapezoidal_integration(lambda x: 4 - x ** 2, 0, 2, 1000)=}")
 
 
 if __name__ == '__main__':
