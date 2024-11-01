5e7a37debd4ea6bb433a58ef67807c3d391d9545
Ivan Mihun <vsk76090@gmail.com>
2024-11-01 15:00:48 +0100

    chore(num_integration): init

---
+++ num_integration/src/main.py
@@ -0,0 +1,18 @@
+def simpson_integration():
+    ...
+
+
+def trapezoidal_integration():
+    ...
+
+
+def rectangular_integration():
+    ...
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
