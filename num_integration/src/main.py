31f485261f6d09b9eb45ca4d8f7c4bf807c3463a
Ivan Mihun <vsk76090@gmail.com>
2024-11-01 15:18:35 +0100

    feat(num_integration): add simpson integration

--- num_integration/src/main.py
+++ num_integration/src/main.py
@@ -1,8 +1,14 @@
 from typing import Callable
 
 
-def simpson_integration():
-    ...
+def simpson_integration(func: Callable[[float], float], a: float, b: float, n: int) -> float:
+    h = (b - a) / n
+    integral = func(a) + func(b)
+    for i in range(1, int(n / 2)):
+        integral += 4 * func(a + (2 * i - 1) * h)
+    for i in range(1, int((n - 2) / 2)):
+        integral += 2 * func(a + (i * 2) * h)
+    return integral * h / 3
 
 
 def trapezoidal_integration(func: Callable[[float], float], a: float, b: float, n: int) -> float:
@@ -24,6 +30,7 @@ def rectangular_integration(func: Callable[[float], float], a: float, b: float, 
 def main():
     print(f"{rectangular_integration(lambda x: 4 - x ** 2, 0, 2, 1000)=}")
     print(f"{trapezoidal_integration(lambda x: 4 - x ** 2, 0, 2, 1000)=}")
+    print(f"{simpson_integration(lambda x: 4 - x ** 2, 0, 2, 1000)=}")
 
 
 if __name__ == '__main__':
