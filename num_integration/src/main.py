4a208b92475c5ab67429f4e559f63851ae14e1c7
Ivan Mihun <vsk76090@gmail.com>
2024-11-01 15:10:34 +0100

    feat(num_integration): add rectangular integration

--- num_integration/src/main.py
+++ num_integration/src/main.py
@@ -1,3 +1,6 @@
+from typing import Callable
+
+
 def simpson_integration():
     ...
 
@@ -6,12 +9,16 @@ def trapezoidal_integration():
     ...
 
 
-def rectangular_integration():
-    ...
+def rectangular_integration(func: Callable[[float], float], a: float, b: float, n: int) -> float:
+    h = (b - a) / n
+    integral = 0
+    for i in range(n):
+        integral += func((a + i * h) + h / 2)
+    return integral * h
 
 
 def main():
-    ...
+    print(f"{rectangular_integration(lambda x: 4 - x ** 2, 0, 2, 1000)=}")
 
 
 if __name__ == '__main__':
