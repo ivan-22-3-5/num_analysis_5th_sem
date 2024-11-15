056cc68d51281d49986466316ca6fa22a2b65eaa
Ivan Mihun <vsk76090@gmail.com>
2024-11-15 12:37:30 +0100

    feat(diff_eq): add runge-kutta method

--- diff_eq/src/main.py
+++ diff_eq/src/main.py
@@ -22,8 +22,24 @@ def euler(func: Callable, y0: float, h: float, rng: tuple[float, float]):
     return build_polynomial(points, 3)
 
 
+def runge_kutta(func: Callable, y0: float, h: float, rng: tuple[float, float]):
+    points: list[Point] = []
+    number_of_points = int((rng[1] - rng[0]) / h)
+    x, y = rng[0], y0
+    for _ in range(number_of_points + 1):
+        points.append(Point(x, y))
+        k1 = func(x, y)
+        k2 = func(x + h / 2, y + k1 * h / 2)
+        k3 = func(x + h / 2, y + k2 * h / 2)
+        k4 = func(x + h, y + k3 * h)
+        x, y = x + h, y + h/6 * (k1 + 2 * k2 + 2 * k3 + k4)
+
+    return build_polynomial(points, 3)
+
+
 def main():
     print(euler(test_func, 1, 0.05, (0, 2)))
+    print(runge_kutta(test_func, 1, 0.05, (0, 2)))
 
 
 if __name__ == '__main__':
